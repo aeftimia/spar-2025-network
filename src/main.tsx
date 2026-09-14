119d698ff48d81c474f30828b282ca574c03902b
Alex Eftimiades <alexeftimiades@gmail.com>
2026-09-13 21:00:27 -0400

    Remove Leaflet stylesheet import

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { MantineProvider, createTheme } from "@mantine/core";
 import "@mantine/core/styles.css";
-import "leaflet/dist/leaflet.css";
 import "./styles.css";
 import App from "./App";
 const theme = createTheme({
